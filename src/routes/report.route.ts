803515147abdf6109a0487c5017df46a287ee1eb
Jedidiah Asare-Nyarko <jedidiah.asare512@gmail.com>
2024-07-25 02:21:05 +0200

    Defined routes for Http methods

---
+++ src/routes/report.route.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import reportController from '../controllers/report.controller';
+
+const reportRouter = express.Router();
+
+reportRouter.get('/special', reportController.specialReport);
+reportRouter.get('/project/:projectId', reportController.findByProjectId);
+
+reportRouter
+	.route('/')
+	.get(reportController.getAll)
+	.post(reportController.create);
+
+reportRouter
+	.route('/:id')
+	.get(reportController.findById)
+	.delete(reportController.remove)
+	.put(reportController.update);
+
+export default reportRouter;
